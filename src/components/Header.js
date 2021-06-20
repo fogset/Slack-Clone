ea0476d2f84b89228906dc26d6fa49e31283dce2
fogset <zhangth1990@hotmail.com>
2021-06-19 23:51:55 -0700

    added google auth login

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,19 @@ import { Avatar } from "@material-ui/core";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import SearchIcon from "@material-ui/icons/Search";
 import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+import { useAuthState } from "react-firebase-hooks/auth"
+import { auth } from "../firebase";
 
 function Header() {
+    const [user, loading] = useAuthState(auth);
+
     return (
         <HeaderContainer>
-
-
             {/*Header left */}
             <HeaderLeft>
                 <HeaderAvatar
-                //TODO: Add onclick
+                    alt={user?.displayName}
+                    src={user?.photoUrl}
                 />
                 <AccessTimeIcon />
             </HeaderLeft>
