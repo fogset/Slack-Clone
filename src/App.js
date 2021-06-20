c2ddf725c5e3bde020ce424a0dc6977b3be7552e
fogset <zhangth1990@hotmail.com>
2021-06-20 04:01:59 -0700

    added loading screen when loading

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import Sidebar from './components/Sidebar.js'
 import Chat from './components/Chat.js'
 import { useAuthState } from "react-firebase-hooks/auth"
+import Spinner from "react-spinkit"
 
 import {
   BrowserRouter as Router,
@@ -18,6 +19,25 @@ import Login from "./components/Login"
 
 function App() {
   const [user, loading] = useAuthState(auth);
+
+  if (loading) {
+    return (
+      <AppLoading>
+        <AppLoadingContents>
+          <img
+            src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg"
+            alt=""
+          />
+
+          <Spinner
+            name="ball-spin-fade-loader"
+            color="purple"
+            fadeIn="none"
+          />
+        </AppLoadingContents>
+      </AppLoading>
+    )
+  }
 
   return (
     <Router>
@@ -42,7 +62,26 @@ function App() {
 
 
 export default App;
+const AppLoading = styled.div`
+display: grid; 
+ place-items: center; 
+height: 100vh;
+width: 100% 
+`
+const AppLoadingContents = styled.div`
+text-align: center;
+padding-bottom: 100px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
 
+> img{
+  height: 100px;
+  padding: 20px;
+  margin-bottom: 40px;
+}
+`
 const AppBody = styled.div`
   display: flex;
   height: 100vh;
